16763863f76e19f8044cd19a5d21dad6e2bb0314
Jacob Hanshaw <hanshaw@wisc.edu>
2012-11-02 21:18:19 -0500

    Added Stuff

---
+++ Bluetooth_Ninja/Ninja/PopOverView.m
@@ -0,0 +1,31 @@
+//
+//  PopOverView.m
+//  Ninja
+//
+//  Created by Jacob Hanshaw on 11/2/12.
+//  Copyright (c) 2012 Jacob Hanshaw. All rights reserved.
+//
+
+#import "PopOverView.h"
+
+@implementation PopOverView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
