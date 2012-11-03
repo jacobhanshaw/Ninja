fb38b3c3ae972977dab5782a8c3d37025ea82d15
Jacob Hanshaw <hanshaw@wisc.edu>
2012-11-03 01:35:16 -0500

    Revert "BLAH"
    This reverts commit 518ae513723b4d2d15c0a51394505d6be6825c81.

--- Bluetooth_Ninja/Ninja/PopOverView.h
+++ Bluetooth_Ninja/Ninja/PopOverView.h
@@ -10,17 +10,11 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface PopOverView : UIView {
-        UILabel     IBOutlet *groupNameLabel;
-        UITextField IBOutlet *groupNameInput;
-        UILabel     IBOutlet *nameLabel;
-        UITextField IBOutlet *nameInput;
-        UIButton    IBOutlet *startGroup;
+        UILabel     *groupNameLabel;
+        UITextField *groupNameInput;
+        UILabel     *nameLabel;
+        UITextField *nameInput;
+        UIButton    *startGroup;
 }
-
-@property (nonatomic) UILabel     IBOutlet *groupNameLabel;
-@property (nonatomic) UITextField IBOutlet *groupNameInput;
-@property (nonatomic) UILabel     IBOutlet *nameLabel;
-@property (nonatomic) UITextField IBOutlet *nameInput;
-@property (nonatomic) UIButton    IBOutlet *startGroup;
 
 @end
